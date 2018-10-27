1d4c80432c49db930deedc1ea0735c732fab3cdf
kaikai <yourkaikai@gmail.com>
2018-10-28 01:57:38 +0800

    work

--- src/components/storyboard.vue
+++ src/components/storyboard.vue
@@ -1,21 +1,44 @@
 <template>
   <div class="section">
-    <div class="storyline">#storyline</div>
-    <div class="board">#board with 2nd level route</div>
+    <div id="storyMode">
+      <div id="chapters">
+        <p>#chapters</p>
+        <div v-for="item in storylevel" :key="item.origin"
+        class="chapter" :class="chapterClass(item)">
+          <div class="cover" :style="chapterCover(item)">#cover</div>
+          <div class="title">{{item.name.CN}}</div>
+        </div>
+        <p>#chapters-end</p>
+      </div>
+      <div class="vo-board">#board with 2nd level route</div>
+    </div>
   </div>
 </template>
 
 <script>
-import levels from './data.js'
+import {levels} from '@/components/data.js'
 export default {
   data () {
     return {
+      storylevel: levels,
+    }
+  },
+  methods:{
+    chapterClass(item){
+      let result={};
+      result["main"]=item.isChapter;
+      return result;
+    },
+    chapterCover(item){
+      let result = {};
+      result["background-image"] = "url(\"static/"+item.cover+"\")";
+      return result;
     }
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 
 </style>
